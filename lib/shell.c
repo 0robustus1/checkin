a0c9b0feae06ce2c986bc9baee714ce559065e52
Tim Reddehase <robustus@rightsrestricted.com>
2014-05-10 22:14:12 +0200

    add basic command parsing including bug-out
    The shell can now be exited (apart from signals)
    through the 'exit' command.
    Start/Stop will get an honorable mention.

--- lib/shell.c
+++ lib/shell.c
@@ -1,22 +1,51 @@
 #include "shell.h"
 
 const int max_line_length = 160;
+const char *in_line_delimiters = " \n";
+const char *prompt = "±> ";
+
 char *line;
 
 void setup_shell();
 void teardown_shell();
+
+int process_line();
 
 void checkin_shell()
 {
   setup_shell();
 
   while(true) {
-    printf("±> ");
-    if (!fgets(line, max_line_length, stdin)) break;
-    printf(line, "\n");
+    printf("%s", prompt);
+    fflush(NULL);
+    if( fgets(line, max_line_length, stdin) ) {
+      if( !process_line() ) break;
+    } else {
+      break;
+    }
   }
 
   teardown_shell();
+}
+
+/*
+ * Returns "boolean" which represents whether the program
+ * should continue.
+ */
+int process_line()
+{
+  char *cmd = strtok(line, in_line_delimiters);
+
+  if( !strcmp(cmd, "exit") ) {
+   return false;
+  } else if( !strcmp(cmd, "start") ) {
+    printf("encountered %s.\n", cmd);
+  } else if( !strcmp(cmd, "stop") ) {
+    printf("encountered %s.\n", cmd);
+  }
+
+  fflush(NULL);
+  return true;
 }
 
 void setup_shell()
