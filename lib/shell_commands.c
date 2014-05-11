574adb402c0d1023301a5820d87b2a0aefdbe40e
Tim Reddehase <robustus@rightsrestricted.com>
2014-05-11 22:54:46 +0200

    add confirmation of storage for timeslot in shell.

--- lib/shell_commands.c
+++ lib/shell_commands.c
@@ -2,6 +2,8 @@
 
 int extract_month_params(int *year, int *month);
 void print_time(struct tm *time, const char *end_with);
+int ask_for_confirmation(char *confirm_for);
+int answer_to_boolean(char *answer);
 
 struct tm *current_start = NULL;
 
@@ -39,6 +41,15 @@ int handle_stop()
     print_time(current_start, "\n");
     printf("End: ");
     print_time(current_stop, "\n");
+
+    int store = ask_for_confirmation("Store the timeslot?");
+
+    if( store ) {
+      printf("Ok, i will store the timeslot...\n");
+    } else {
+      printf("Storing of timeslot aborted.\n");
+    }
+
     free(current_start);
     current_start = NULL;
     free(current_stop);
@@ -125,4 +136,57 @@ void print_time(struct tm *time, const char *end_with)
   printf("%s%s", time_string, end_with);
 
   free(time_string);
+}
+
+/*
+ * Asks for confirmation.
+ * Returns boolean value respective to yes/no response.
+ * confirm_for: used as a message to user
+ */
+int ask_for_confirmation(char *confirm_for)
+{
+  int max_answer_length = 4;
+  int unanswered = true;
+  int boolean_answer = false;
+  char *raw_answer = (char *) malloc(max_answer_length * sizeof(char));
+  while(unanswered) {
+    if( confirm_for ) {
+      printf("%s\n", confirm_for);
+    }
+    printf("Confirm y/n: ");
+    fflush(NULL);
+    if( fgets(raw_answer, max_answer_length, stdin) ) {
+      char *answer = strtok(raw_answer, in_line_delimiters);
+      boolean_answer = answer_to_boolean(answer);
+      if( boolean_answer == -1 ) {
+        printf("Please provide either 'y' or 'n'.\n");
+      } else {
+        unanswered = false;
+      }
+    } else {
+      break;
+    }
+  }
+  free(raw_answer);
+  return boolean_answer;
+}
+
+/*
+ * Converts an answer-string into a boolean response.
+ * "y" or "yes" will yield true.
+ * "n" or "no" will yield false.
+ * Every other value of answer will yield -1, which
+ * denotes an invalid answer.
+ */
+int answer_to_boolean(char *answer) {
+  int boolean = false;
+  if( !answer ) return -1;
+  if( !strcmp(answer, "y") || !strcmp(answer, "yes") ) {
+    boolean = true;
+  } else if( !strcmp(answer, "n") || !strcmp(answer, "no") ) {
+    boolean = false;
+  } else {
+    boolean = -1;
+  }
+  return boolean;
 }
