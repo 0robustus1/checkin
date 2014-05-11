39e91ed5e33702d08f1b7be976310bc080997959
Tim Reddehase <robustus@rightsrestricted.com>
2014-05-11 13:48:40 +0200

    refactor date handling for list-cmd in shell.

--- lib/shell.c
+++ lib/shell.c
@@ -12,6 +12,7 @@ void teardown_shell();
 int process_line();
 void printNow();
 struct tm * retrieve_now(struct tm *storage);
+int extract_month_params(int *year, int *month);
 
 void checkin_shell()
 {
@@ -50,32 +51,72 @@ int process_line()
     struct tm now;
     struct tm *now_p = retrieve_now(&now);
 
-    // First part of month/year syntax
-    char *param = strtok(NULL, "/ \n");
-    if( param ) {
-      int month;
-      if( sscanf(param, "%d", &month) != 1) {
-        printf("Expected valid `month/year` or `month` syntax.");
-      } else {
-        param = strtok(NULL, in_line_delimiters);
-        if( param ) {
-          int year;
-          if( sscanf(param, "%d", &year) != 1) {
-            printf("Expected valid `month/year` syntax.");
-          } else {
-            checkin_list(db_handler, now_p, &year, &month);
-          }
-        } else {
-          checkin_list(db_handler, now_p, NULL, &month);
-        }
-      }
-    } else {
-      checkin_list(db_handler, now_p, NULL, NULL);
+    int *year_p = (int *) malloc( sizeof(int) );
+    int *month_p = (int *) malloc( sizeof(int) );
+
+    int success = extract_month_params(year_p, month_p);
+
+    switch( success ) {
+      case -1:
+      case 0:
+        free(year_p);
+        free(month_p);
+        month_p = NULL;
+        year_p = NULL;
+        break;
+      case 1:
+        free(year_p);
+        year_p = NULL;
+        break;
+      default:
+        break;
     }
+
+    if( success ) {
+      checkin_list(db_handler, now_p, year_p, month_p);
+    }
+
+    free(month_p);
+    free(year_p);
+
+
   }
 
   fflush(NULL);
   return true;
+}
+
+/*
+ * Returns false (zero), if errors were encountered.
+ * Returns 1 if number of arguments is 1 (only month).
+ * Returns 2 if number of arguments is 2 (month and year).
+ * Returns -1 if no arguments were encountered.
+ */
+int extract_month_params(int *year_p, int *month_p)
+{
+  // First part of month/year syntax
+  char *param = strtok(NULL, "/ \n");
+
+  if( param ) {
+    if( sscanf(param, "%d", month_p) != 1) {
+      printf("Expected valid `month/year` or `month` syntax.");
+      return false;
+    } else {
+      param = strtok(NULL, in_line_delimiters);
+      if( param ) {
+        if( sscanf(param, "%d", year_p) != 1) {
+          printf("Expected valid `month/year` syntax.");
+          return false;
+        } else {
+          return 2;
+        }
+      } else {
+        return 1;
+      }
+    }
+  } else {
+    return -1;
+  }
 }
 
 void printNow()
